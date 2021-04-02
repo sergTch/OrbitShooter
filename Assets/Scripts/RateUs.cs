21aa7393729d7a029f8bfa0f3912e4fdec73c89c
VoVa <Svova9@gmail.com>
2021-04-02 22:09:51 +0300

    sound + canvas

---
+++ Assets/Scripts/RateUs.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RateUs : MonoBehaviour
+{
+
+private var Link;
+
+#if UNITY_ANDROID
+    private const string Link = "https://answers.unity.com/questions/1116891/onclick-open-url.html"; 
+#elif UNITY_IPHONE
+    private const string Link = "https://answers.unity.com/questions/1116891/onclick-open-url.html";
+#else
+    private const string Link = "unexpected_platform";
+#endif
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RateUs() {
+
+Application.OpenURL(Link);
+
+    }
+}
